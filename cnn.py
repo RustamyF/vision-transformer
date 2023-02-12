0789d1047ee597e5f69740f4ab2672c1bead0118
Fahim Rustamy <faheemrustamy@gmail.com>
2023-02-12 16:31:33 -0500

    updated the formating

--- cnn.py
+++ cnn.py
@@ -1,19 +1,13 @@
 import torch.nn as nn
 import torch
 import torch.optim as optim
-import torch.nn.functional as F
 
 from torchvision import datasets, models, transforms
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 from sklearn.model_selection import train_test_split
-#using numpy
-import numpy as np
 
-#check our work directory
 import os
-
-
 
 
 class LoadData:
@@ -44,8 +38,7 @@ class LoadData:
         cat_list = os.listdir(self.cat_path)
         cat_list = [(os.path.join(self.cat_path, i), 0) for i in cat_list]
 
-        total_list = cat_list+dog_list
-
+        total_list = cat_list + dog_list
 
         train_list, test_list = train_test_split(total_list, test_size=0.2)
         train_list, val_list = train_test_split(train_list, test_size=0.2)
@@ -55,115 +48,105 @@ class LoadData:
         return train_list, test_list, val_list
 
 
-
-#data Augumentation
-transform =  transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-    ])
-
+# data Augumentation
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
 
 
 class dataset(torch.utils.data.Dataset):
-    
-    def __init__(self,file_list,transform=None):
+
+    def __init__(self, file_list, transform=None):
         self.file_list = file_list
         self.transform = transform
-        
-        
-    #dataset length
+
+    # dataset length
     def __len__(self):
         self.filelength = len(self.file_list)
         return self.filelength
-    
-    #load an one of images
-    def __getitem__(self,idx):
+
+    # load an one of images
+    def __getitem__(self, idx):
         img_path, label = self.file_list[idx]
         img = Image.open(img_path).convert('RGB')
         img_transformed = self.transform(img)
-        return img_transformed,label
-    
+        return img_transformed, label
 
 
 class Cnn(nn.Module):
     def __init__(self):
-        super(Cnn,self).__init__()
-        
+        super(Cnn, self).__init__()
+
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3,16,kernel_size=3, padding=0,stride=2),
+            nn.Conv2d(3, 16, kernel_size=3, padding=0, stride=2),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        
+
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16,32, kernel_size=3, padding=0, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, padding=0, stride=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2)
-            )
-        
+        )
+
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32,64, kernel_size=3, padding=0, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=0, stride=2),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        
-        
-        self.fc1 = nn.Linear(3*3*64,10)
+
+        self.fc1 = nn.Linear(3 * 3 * 64, 10)
         self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(10,2)
+        self.fc2 = nn.Linear(10, 2)
         self.relu = nn.ReLU()
-        
-        
-    def forward(self,x):
+
+    def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = out.view(out.size(0),-1)
+        out = out.view(out.size(0), -1)
         out = self.relu(self.fc1(out))
         out = self.fc2(out)
         return out
 
 
-    
-    
-    
 if __name__ == "__main__":
-    lr = 0.001 # learning_rate
-    batch_size = 100 # we will use mini-batch method
-    epochs = 10 # How much to train a model
+    lr = 0.001  # learning_rate
+    batch_size = 100  # we will use mini-batch method
+    epochs = 10  # How much to train a model
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     torch.manual_seed(1234)
-    if device =='cuda':
+    if device == 'cuda':
         torch.cuda.manual_seed_all(1234)
 
     print(device)
-    
+
     load_data = LoadData()
-    
+
     train_list, test_list, val_list = load_data.data()
-    
-    
+
     train_data = dataset(train_list, transform=transform)
     test_data = dataset(test_list, transform=transform)
     val_data = dataset(val_list, transform=transform)
 
-    train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size=batch_size, shuffle=False )
-    test_loader = torch.utils.data.DataLoader(dataset = test_data, batch_size=batch_size, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(dataset = val_data, batch_size=batch_size, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(dataset=val_data, batch_size=batch_size, shuffle=True)
 
     model = Cnn().to(device)
     model.train()
 
-    optimizer = optim.Adam(params = model.parameters(),lr=0.001)
+    optimizer = optim.Adam(params=model.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss()
-    
+
     for epoch in range(epochs):
         epoch_loss = 0
         epoch_accuracy = 0
@@ -180,25 +163,23 @@ if __name__ == "__main__":
             optimizer.step()
 
             acc = ((output.argmax(dim=1) == label).float().mean())
-            epoch_accuracy += acc/len(train_loader)
-            epoch_loss += loss/len(train_loader)
+            epoch_accuracy += acc / len(train_loader)
+            epoch_loss += loss / len(train_loader)
 
-        print('Epoch : {}, train accuracy : {}, train loss : {}'.format(epoch+1, epoch_accuracy,epoch_loss))
-
+        print('Epoch : {}, train accuracy : {}, train loss : {}'.format(epoch + 1, epoch_accuracy, epoch_loss))
 
         with torch.no_grad():
-            epoch_val_accuracy=0
-            epoch_val_loss =0
+            epoch_val_accuracy = 0
+            epoch_val_loss = 0
             for data, label in val_loader:
                 data = data.to(device)
                 label = label.to(device)
 
                 val_output = model(data)
-                val_loss = criterion(val_output,label)
-
+                val_loss = criterion(val_output, label)
 
                 acc = ((val_output.argmax(dim=1) == label).float().mean())
-                epoch_val_accuracy += acc/ len(val_loader)
-                epoch_val_loss += val_loss/ len(val_loader)
+                epoch_val_accuracy += acc / len(val_loader)
+                epoch_val_loss += val_loss / len(val_loader)
 
-            print('Epoch : {}, val_accuracy : {}, val_loss : {}'.format(epoch+1, epoch_val_accuracy,epoch_val_loss))
+            print('Epoch : {}, val_accuracy : {}, val_loss : {}'.format(epoch + 1, epoch_val_accuracy, epoch_val_loss))
--- vit.py
+++ vit.py
@@ -1,16 +1,12 @@
 import torch.nn as nn
 import torch
 import torch.optim as optim
-import torch.nn.functional as F
 
 from torchvision import datasets, models, transforms
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 from sklearn.model_selection import train_test_split
-#using numpy
-import numpy as np
 
-#check our work directory
 import os
 
 from linformer import Linformer
@@ -19,7 +15,7 @@ from vit_pytorch.efficient import ViT
 
 
 class LoadData:
-    
+
     def __init__(self):
         self.cat_path = 'kagglecatsanddogs_3367a/PetImages/Cat'
         self.dog_path = 'kagglecatsanddogs_3367a/PetImages/Dog'
@@ -47,8 +43,7 @@ class LoadData:
         cat_list = os.listdir(self.cat_path)
         cat_list = [(os.path.join(self.cat_path, i), 0) for i in cat_list]
 
-        total_list = cat_list+dog_list
-
+        total_list = cat_list + dog_list
 
         train_list, test_list = train_test_split(total_list, test_size=0.2)
         train_list, val_list = train_test_split(train_list, test_size=0.2)
@@ -58,48 +53,42 @@ class LoadData:
         return train_list, test_list, val_list
 
 
-
-#data Augumentation
-transform =  transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-    ])
-
+# data Augumentation
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
 
 
 class dataset(torch.utils.data.Dataset):
-    
-    def __init__(self,file_list,transform=None):
+
+    def __init__(self, file_list, transform=None):
         self.file_list = file_list
         self.transform = transform
-        
-    #dataset length
+
+    # dataset length
     def __len__(self):
         self.filelength = len(self.file_list)
         return self.filelength
-    
-    #load an one of images
-    def __getitem__(self,idx):
+
+    # load an one of images
+    def __getitem__(self, idx):
         img_path, label = self.file_list[idx]
         img = Image.open(img_path).convert('RGB')
         img_transformed = self.transform(img)
-        return img_transformed,label
-    
+        return img_transformed, label
 
 
 efficient_transformer = Linformer(
     dim=128,
-    seq_len=49+1,  # 7x7 patches + 1 cls-token
+    seq_len=49 + 1,  # 7x7 patches + 1 cls-token
     depth=12,
     heads=8,
     k=64
 )
 
-
-
-    
 if __name__ == "__main__":
     # Training settings
     batch_size = 64
@@ -111,23 +100,22 @@ if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     torch.manual_seed(1234)
-    if device =='cuda':
+    if device == 'cuda':
         torch.cuda.manual_seed_all(1234)
 
     print(device)
-    
+
     load_data = LoadData()
-    
+
     train_list, test_list, val_list = load_data.data()
-    
-    
+
     train_data = dataset(train_list, transform=transform)
     test_data = dataset(test_list, transform=transform)
     val_data = dataset(val_list, transform=transform)
 
-    train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size=batch_size, shuffle=False )
-    test_loader = torch.utils.data.DataLoader(dataset = test_data, batch_size=batch_size, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(dataset = val_data, batch_size=batch_size, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(dataset=val_data, batch_size=batch_size, shuffle=True)
 
     model = ViT(
         dim=128,
@@ -146,7 +134,7 @@ if __name__ == "__main__":
     scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
 
     epochs = 10
-    
+
     for epoch in range(epochs):
         epoch_loss = 0
         epoch_accuracy = 0
@@ -163,35 +151,23 @@ if __name__ == "__main__":
             optimizer.step()
 
             acc = ((output.argmax(dim=1) == label).float().mean())
-            epoch_accuracy += acc/len(train_loader)
-            epoch_loss += loss/len(train_loader)
+            epoch_accuracy += acc / len(train_loader)
+            epoch_loss += loss / len(train_loader)
 
-        print('Epoch : {}, train accuracy : {}, train loss : {}'.format(epoch+1, epoch_accuracy,epoch_loss))
-
+        print('Epoch : {}, train accuracy : {}, train loss : {}'.format(epoch + 1, epoch_accuracy, epoch_loss))
 
         with torch.no_grad():
-            epoch_val_accuracy=0
-            epoch_val_loss =0
+            epoch_val_accuracy = 0
+            epoch_val_loss = 0
             for data, label in val_loader:
                 data = data.to(device)
                 label = label.to(device)
 
                 val_output = model(data)
-                val_loss = criterion(val_output,label)
-
+                val_loss = criterion(val_output, label)
 
                 acc = ((val_output.argmax(dim=1) == label).float().mean())
-                epoch_val_accuracy += acc/ len(val_loader)
-                epoch_val_loss += val_loss/ len(val_loader)
+                epoch_val_accuracy += acc / len(val_loader)
+                epoch_val_loss += val_loss / len(val_loader)
 
-            print('Epoch : {}, val_accuracy : {}, val_loss : {}'.format(epoch+1, epoch_val_accuracy,epoch_val_loss))
-
-
-
-
-
-
-    
-
-
-
+            print('Epoch : {}, val_accuracy : {}, val_loss : {}'.format(epoch + 1, epoch_val_accuracy, epoch_val_loss))
